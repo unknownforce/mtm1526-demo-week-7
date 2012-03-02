4e0cefbe9cef92729d199486ff369b87348dfde4
unknownforce <petchan34@gmail.com>
2012-03-02 12:25:07 -0500

    added AJAX todo application

---
+++ ajax-todo/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Todo</title>
+</head>
+
+<body>
+	
+	<form>
+    	<label for="item">Item</label>
+    	<input id="item">
+        <button type="submit">Add</button>
+    </form>
+
+	<ul class="todos"></ul>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="js/todo.js"></script>
+
+
+</body>
+</html>
---
+++ ajax-todo/js/todo.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	
+	// Immediately request the list of todos and inserts them into the UL
+	$('.todos').load('list.php');	
+	
+});
